ca886ceae88cb0431e5f6b76acffc76aebe441b6
amarvenna <amareswarareddyvenna2001@gmail.com>
2024-01-01 17:23:00 +0530

    power using efficient recursion method

---
+++ recursion/power2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<stdio.h>
+
+using namespace std;
+
+int pow(int m,int n)
+{
+	if(n==0)
+		return 1;
+	if(n%2==0)
+		return pow(m*m,n/2);
+	else
+		return m*pow(m*m,(n-1)/2);
+}
+int main()
+{
+	cout<<pow(2,6)<<endl;
+	return 0;
+}
