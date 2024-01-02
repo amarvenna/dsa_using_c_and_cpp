9ae12ecc91f2e92f15cf70087455a84256b2f553
amarvenna <amareswarareddyvenna2001@gmail.com>
2024-01-02 17:17:32 +0530

    taylor series using horner's rule

---
+++ recursion/taylorSeries2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+// taylor series using horner's method
+
+double e(int x,int n)
+{
+	static double s=1;
+	if(n==0)
+		return s;
+	s=1+x*s/n;
+	return e(x,n-1);
+}
+int main()
+{
+	cout<<e(1,10)<<endl;
+	return 0;
+}
