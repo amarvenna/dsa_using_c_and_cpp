5fdd0ade009ae35d78560be943644afdb9675508
amarvenna <amareswarareddyvenna2001@gmail.com>
2024-01-02 16:07:40 +0530

    using excessive recursion method

---
+++ recursion/fibonacci.cpp
@@ -0,0 +1,17 @@
+// using excessive recursion approach
+
+#include<iostream>
+
+using namespace std;
+
+int fib(int n)
+{
+	if(n<=1) return n;
+	return fib(n-2)+fib(n-1);
+}
+int main()
+{
+	cout<<fib(5)<<endl;
+	return 0;
+}
+       
