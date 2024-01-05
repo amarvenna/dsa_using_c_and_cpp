0c5342847ace369394f3d8b724109ac10602af34
amarvenna <amareswarareddyvenna2001@gmail.com>
2024-01-05 21:48:52 +0530

    binary search using iterative version

---
+++ arrayADT/binarySearch.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+struct Array
+{
+	int A[10];
+	int size;
+	int length;
+};
+
+int BinarySearch(struct Array arr, int key)
+{
+	int l,mid,h;
+	l=0;
+	h=arr.length-1;
+	while(l<=h)
+	{
+		mid=(l+h)/2;
+		if(key == arr.A[mid])
+			return mid;
+		else if(key < arr.A[mid])
+			h = mid-1;
+		else
+			l = mid+1;
+	}
+	return -1;
+}
+
+int main()
+{
+	struct Array arr = {{2,3,4,5,6},10,5};
+	printf("The index is: %d\n",BinarySearch(arr,5));
+	return 0;
+}
